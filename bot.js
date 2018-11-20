f551f985ceb5fc3d644e2da0006469e660fcab11
iiHouSSaM <44523796+iiHouSSaM@users.noreply.github.com>
2018-11-20 21:07:48 +0100

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-    client.user.setGame(`~`,'https://www.twitch.tv/MeeRcY')
-client.user.setStatus("dnd")
-    
-});
-client.login(process.env.BOT_TOKEN);
